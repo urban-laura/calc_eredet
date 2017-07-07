e03b5ea4fb140791f996c602aa67b04aeb53bd39
urban-laura <urban.laura97@gmail.com>
2017-07-07 14:48:49 +0200

    div finished

--- templates/form-table.tpl.php
+++ templates/form-table.tpl.php
@@ -6,104 +6,58 @@
 <script src="js/atiras.js"></script>
 </head>
 
-<table class='title'>
-<tr>
-<td>Eredetvizsgálati díj</td>
-</tr>
-</table>
+<body>
 
-<table class='center'>
-<tr>
-<td>Hatósági díjak: </td>
-</tr>
+<div class='frame'>
 
-<form>
+<div class='title'>
+<h1>Eredetvizsgálati díj</h1>
+</div>
 
-<tr>
-<td>
+<div class='center'>
+
+<h4>Hatósági díjak: </h4>
 <select name = 'hatosagi_dijak' id='hatosagi_dijak' class='dijak'>
 	###hatosagi_dijak###
 </select>
-</td>
-</tr>
-</form>
-</table>
+</div>
 
-<table class='center' id='szemelygepjarmuvek'>
-<tr>
-<td>a) Kis kategória (1400 cm3 hengerűrtartalomig) - 17 000 Ft</td>
-</tr>
+<div class='center' id='szemelygepjarmuvek'>
+<h3>a) Kis kategória (1400 cm3 hengerűrtartalomig) - 17 000 Ft</h3>
+<h3>b) Közép kategória (1401-2000 cm3 hengerűrtartalomig) - 18 500 Ft</h3>
+<h3>c) Felső kategória (2001 cm3 hengerűrtartalom felett) - 20 000 Ft</h3>
+</div>
 
-<tr>
-<td>b) Közép kategória (1401-2000 cm3 hengerűrtartalomig) - 18 500 Ft</td>
-</tr>
+<div class='center' id='motorkerekparok'>
+<h3>a) 500 cm3 hengerűrtartalomig - 15 500 Ft</h3>
+<h3>b) 500 cm3 hengerűrtartalom felett - 17 000 Ft</h3>
+<h3>c) Négykerekű segédmotoros kerékpár - 14 000 Ft</h3>
+</div>
 
-<tr>
-<td>c) Felső kategória (2001 cm3 hengerűrtartalom felett) - 20 000 Ft</td>
-</tr>
-</table>
+<div class='center' id='kisteherautok'>
+<h3>a) Kisteherautók (3,5t össztömegig) - 20 000 Ft</h3>
+</div>
 
-<table class='center' id='motorkerekparok'>
-<tr>
-<td>a) 500 cm3 hengerűrtartalomig - 15 500 Ft</td>
-</tr>
+<div class='center' id='tehergepkocsi'>
+<h3>a) 3,5 és 7,5 t össztömegig - 21 000 Ft</h3>
+<h3>b) 7,5 t össztömeg felett - 22 000 Ft</h3>
+</div>
 
-<tr>
-<td>b) 500 cm3 hengerűrtartalom felett - 17 000 Ft</td>
-</tr>
+<div class='center' id='autobuszok'>
+<h3>a) 20 fő a szállítható személyek maximuma - 21 000 Ft</h3>
+<h3>b) 20 fő feletti szállítható személyek esetén - 22 000 Ft</h3>
+</div>
 
-<tr>
-<td>c) Négykerekű segédmotoros kerékpár - 14 000 Ft</td>
-</tr>
-</table>
+<div class='center 'id='mezogazdasagi'>
+<h3>a) Mezőgazdasági vontató, lassú jármű - 20 000 Ft</h3>
+</div>
 
-<table class='center' id='kisteherautok'>
-<tr>
-<td>a) Kisteherautók (3,5t össztömegig) - 20 000 Ft</td>
-</tr>
-</table>
+<div class='center' id='potkocsi'>
+<h3>a) Könnyű pótkocsi (lakókocsi) - 16 000 Ft</h3>
+<h3>b) Nehéz pótkocsi - 17 500 Ft</h3>
+<h3>c) Különleges pótkocsi (felépítménnyel ellátott) - 19 000 Ft</h3>
+</div>
 
-<table class='center' id='tehergepkocsi'>
-<tr>
-<td>a) 3,5 és 7,5 t össztömegig - 21 000 Ft</td>
-</tr>
+</div>
 
-<tr>
-<td>b) 7,5 t össztömeg felett - 22 000 Ft</td>
-</tr>
-</table>
-
-<table class='center' id='autobuszok'>
-<tr>
-<td>a) 20 fő a szállítható személyek maximuma - 21 000 Ft</td>
-</tr>
-
-<tr>
-<td>b) 20 fő feletti szállítható személyek esetén - 22 000 Ft</td>
-</tr>
-</table>
-
-<table class='center 'id='mezogazdasagi'>
-<tr>
-<td>a) Mezőgazdasági vontató, lassú jármű - 20 000 Ft</td>
-</tr>
-</table>
-
-<table class='center' id='potkocsi'>
-<tr>
-<td>a) Könnyű pótkocsi (lakókocsi) - 16 000 Ft</td>
-</tr>
-
-<tr>
-<td>b) Nehéz pótkocsi - 17 500 Ft</td>
-</tr>
-
-<tr>
-<td>c) Különleges pótkocsi (felépítménnyel ellátott) - 19 000 Ft</td>
-</tr>
-
-<table class='zero'>
-<tr>
-<td></td>
-</tr>
-</table>
+</body>
